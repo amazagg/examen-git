26c2822e11734171341d93795d279f7ed7c56b9e
AMAZIGH AGGUINI <amazighagguini@yahoo.com>
2025-11-28 10:50:13 -0500

    Ajout du dossier exercice et du fichier test.js

---
+++ exercice/test.js
@@ -0,0 +1,12 @@
+
+// Auteur : AMAZIGH
+
+console.log("Bienvenue dans mon fichier test.js !");
+
+// Fonction 
+function saluer(nom) {
+    return `Bonjour ${nom}, ceci est un test Git !`;
+}
+
+// Appel de la fonction
+console.log(saluer("AMAZIGH"));
